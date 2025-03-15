ac25e536e48372d60e40cdc7d2acfd236a8080c8
SergeyVSwift <vashhenkobs@gmail.com>
2025-03-15 19:52:59 +0100

    Initial Commit

---
+++ BookMaster/BookMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookMasterApp.swift
+//  BookMaster
+//
+//  Created by Sergey V on 15.03.25.
+//
+
+import SwiftUI
+
+@main
+struct BookMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
