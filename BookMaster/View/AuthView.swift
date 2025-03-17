28e16a93fe557ecb4069fab4178d5b6acd897c3c
SergeyVSwift <vashhenkobs@gmail.com>
2025-03-17 22:54:22 +0100

    AuthView

---
+++ BookMaster/View/AuthView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthView.swift
+//  BookMaster
+//
+//  Created by Sergey V on 17.03.25.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthView()
+}
